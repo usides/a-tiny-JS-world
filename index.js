aead7d731d695ba41b0cf6ad87f115b23537afe1
usides <usides@gmail.com>
2020-11-09 17:56:22 +0400

    composition style

--- index.js
+++ index.js
@@ -1,46 +1,80 @@
-class Inhabitant {
-  constructor(species, name, legs, hands, gender, saying) {
-    this.species = species;
-    this.name = name;
-    this._legs = legs;
-    this.gender = gender;
-    this.saying = saying;
-    this.constants = {
-      props: ['species', 'name', 'gender', 'legs', 'saying'],
-    };
-  }
+function createResident({ species, legs, name, gender, saying, friends = 'No friends' }) {
+  const props = { species, legs, name, gender, saying, friends };
+  return {
+    ...props,
+    ...toString(),
+  };
+}
 
+const toString = () => ({
   toString() {
-    return ['species', 'name', 'gender', 'legs', 'saying']
-      .map(prop => this[prop]).join('; ');
-  }
+    return Object.entries(this)
+      .filter((item) => typeof item[1] !== 'function')
+      .map((item) => `${item[0]}: ${item[1]};`)
+      .join(' ');
+  },
+});
+
+function createCat({ species = 'cat', legs = 4, name, gender, friends }) {
+  return {
+    ...createResident({ species, legs, name, gender, friends }),
+    ...catSay(),
+  };
 }
 
-class Dog extends Inhabitant {
-  constructor(name, gender, legs = 4, saying = 'woof') {
-    super('dog', name, legs, gender, saying);
-  }
+function catSay() {
+  return {
+    saying: 'meow-meow',
+  };
 }
 
-class Human extends Inhabitant {
-  constructor(name, gender, saying) {
-    super('human', name, 2, gender, saying);
-    this.hands = 2;
-  }
-
-  toString() {
-    return super.toString() + this.hands + '; ';
-  }
+function createDog({ species = 'dog', legs = 4, saying = 'bark-bark', name, gender, friends }) {
+  return {
+    ...createResident({ species, legs, name, gender, friends, saying }),
+  };
 }
 
-const heDog = new Dog('Bolt',
-   'male', 4, 'GAFF');
-const man = new Human('Jake', 'male', 'Horray!');
-const woman = new Human('Jane', 'female', 'Mee-mee-mee!');
-
-[man, woman, heDog].forEach(printElement);
-
-
-function printInhabitant(inhabitant) {
-  inhabitant.toString();
+function createHuman({ species = 'human', legs = 2, hands = 2, saying, name, gender, friends }) {
+  return {
+    ...createResident({ species, legs, name, gender, friends, saying }),
+    ...{ hands },
+  };
 }
+
+function createCatWoman({  species = 'cat-woman',  legs,  hands, name, gender = 'female', friends }) {
+  return {
+    ...createHuman({ species, legs, hands, name, gender, friends }),
+    ...catSay(),
+  };
+}
+
+const cat = createCat({
+  name: 'Meowler',
+  gender: 'male',
+  friends: 'Dude, Alice, Fur',
+});
+const dog = createDog({
+  name: 'Doggy',
+  gender: 'male',
+  friends: 'Alice',
+});
+const man = createHuman({
+  name: 'Dude',
+  gender: 'male',
+  saying: "Hey! What's up?",
+  friends: 'Alice, Meowler',
+});
+const woman = createHuman({
+  name: 'Alice',
+  gender: 'female',
+  saying: 'What a nice place!',
+  friends: 'Dude, Meowler, Doggy',
+});
+const catWoman = createCatWoman({
+  name: 'Fur',
+  friends: 'Meowler',
+});
+
+const inhabitants = [dog, cat, man, woman, catWoman];
+
+inhabitants.forEach((elem) => printElement(elem.toString()));
